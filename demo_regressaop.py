da8905ffaa9b72ef2374a068d3c7fcf62fda8cfd
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2019-11-16 12:47:16 -0300

    Desenvolvido para apresentar gráfico com os dados do x e y

--- demo_regressaop.py
+++ demo_regressaop.py
@@ -1,9 +1,28 @@
+from collections import namedtuple
+import matplotlib.pyplot as plt
 import pandas as pd
 
 class Questao03:
 
     def __init__(self):
-        pass
+        self.vetor_x = []
+        self.vetor_y = []
+
+        self.definir_variaveis(self.ler_dados())
+        self.gerar_grafico()
+
+
+    def definir_variaveis(self, dados):
+        for i, array in enumerate(dados.values):
+            self.vetor_x.append(array[0])
+            self.vetor_y.append(array[1])
+
+    def ler_dados(self):
+        return pd.read_csv('.\/assets\/Dados\/data_preg.csv')
+
+    def gerar_grafico(self):
+        plt.scatter(self.vetor_x, self.vetor_y, marker='s',  color='red', linewidth=1)
+        plt.show()
 
 
 if __name__ == "__main__":
