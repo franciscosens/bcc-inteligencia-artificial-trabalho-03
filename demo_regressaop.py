c1d3d53491c9c0a2103c0e486e07e0b808fb794f
Gustavo Westarb <gustavowestarb45@gmail.com>
2019-11-17 22:00:15 -0300

    Feito as linhas da regressao, falta apenas responder as questoes, G e K (a K eh obvia...)

--- demo_regressaop.py
+++ demo_regressaop.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 import matplotlib.pyplot as plt
+import numpy as np
+import numpy.polynomial.polynomial as poly
 import pandas as pd
 
 class Questao03:
@@ -12,16 +14,47 @@ class Questao03:
         self.gerar_grafico()
 
 
+    def gerar_polyfit(self, valor_N):
+        return np.polyfit(self.vetor_x, self.vetor_y, valor_N)[::-1]
+
+
+    def gerar_linha(self, valor_n_regressao):
+        novo_vetor_x = np.linspace(self.vetor_x[0], self.vetor_x[-1], num=len(self.vetor_x)*10)
+        novo_vetor_y = poly.polyval(novo_vetor_x, valor_n_regressao)
+
+        Retorno_linha = namedtuple("Retorno", ["vetor_x", "vetor_y"])
+        return Retorno_linha(novo_vetor_x, novo_vetor_y)
+
+
     def definir_variaveis(self, dados):
         for i, array in enumerate(dados.values):
             self.vetor_x.append(array[0])
             self.vetor_y.append(array[1])
 
+
     def ler_dados(self):
         return pd.read_csv('.\/assets\/Dados\/data_preg.csv')
 
+
     def gerar_grafico(self):
         plt.scatter(self.vetor_x, self.vetor_y, marker='s',  color='red', linewidth=1)
+
+        #Questao - C
+        linha_C = self.gerar_linha(self.gerar_polyfit(1))
+        plt.plot(linha_C.vetor_x, linha_C.vetor_y, color='red')
+
+        #Questao - D
+        linha_D = self.gerar_linha(self.gerar_polyfit(2))
+        plt.plot(linha_D.vetor_x, linha_D.vetor_y, color='green')
+        
+        #Questao - E
+        linha_E = self.gerar_linha(self.gerar_polyfit(3))
+        plt.plot(linha_E.vetor_x, linha_E.vetor_y, color='black')
+
+        #Questao - F
+        linha_F = self.gerar_linha(self.gerar_polyfit(4))
+        plt.plot(linha_F.vetor_x, linha_F.vetor_y, color='yellow')
+
         plt.show()
 
 
